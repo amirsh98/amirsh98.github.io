201d1bc18bc852fa1d2a38458551499d52619c78
Amirhossein Jamshidpour <Mr.jamshidpour@gmail.com>
2026-09-06 10:14:51 +0330

    Remove basePath for user-site hosting at amirsh98.github.io

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/jamshidpour.github.io",
   trailingSlash: true,
   images: {
     unoptimized: true,
